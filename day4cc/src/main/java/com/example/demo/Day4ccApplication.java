c21124c2b591b49fa40545ed01c87641bc1f6fc6
Dalui-chingu <125961514+Dalui-chingu@users.noreply.github.com>
2023-08-23 10:34:22 +0530

    Add files via upload

---
+++ day4cc/src/main/java/com/example/demo/Day4ccApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4ccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4ccApplication.class, args);
+	}
+
+}
